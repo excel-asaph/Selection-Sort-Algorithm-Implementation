53998715a2c9bcfeee0a96d9a1dce9e679ab064e
Excel Asaph <151797298+excel-asaph@users.noreply.github.com>
2023-12-13 00:53:13 +0200

    Add header file for Selection Sort algorithm

---
+++ selectionsort.h
@@ -0,0 +1,22 @@
+#ifndef SELECTIONSORT_H
+#define SELECTIONSORT_H
+
+#include <stdio.h>
+
+/**
+ * @file selectionsort.h
+ * @brief Header file for the Selection Sort algorithm.
+ */
+
+/**
+ * @brief Sorts an array in ascending order using the Selection Sort algorithm.
+ *
+ * This function implements the Selection Sort algorithm to sort the input array in
+ * ascending order.
+ *
+ * @param arr An array to be sorted.
+ * @param size The size of the array.
+ */
+void selectionsort(int arr[], int size);
+
+#endif
